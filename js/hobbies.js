5a46c3a09952954210fc8ff1e25fcdbeb8109456
Alex Mihov <alexmihov.as@gmail.com>
2025-01-31 13:55:11 +0100

    [feat] hobbies.js function

---
+++ js/hobbies.js
@@ -0,0 +1,7 @@
+// creazione funzione per hobbies 
+function createHobby(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+const hobbies = createHobby("Calcio", "Viaggare", "Leggere")
+console.log(hobbies);
